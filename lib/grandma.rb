8ca03e741af27c47468d5e37db133cdd294760dc
Chloe Marie <chloepuck13@yahoo.com>
2017-06-06 20:57:13 +0000

    Grandpa Speaking Finished

--- lib/grandma.rb
+++ lib/grandma.rb
@@ -1,2 +1,21 @@
-# Write your code here!
+def speak_to_grandma (speak)
+    if speak==speak.upcase
+        "NO, NOT SINCE #{rand(1930..1950)}"
+    else 
+        "HUH?! SPEAK UP SONNY!"
+    end 
+end 
+
+speak="0"
+i=0
+
+while i!=3 do
+puts "What do you want to say to grandma?"
+speak = gets.chomp
+puts speak_to_grandma(speak)
+if speak=="BYE!"
+        i=i+1
+end
+end
+
 
